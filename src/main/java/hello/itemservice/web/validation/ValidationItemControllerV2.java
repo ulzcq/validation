7ce638d895e227910c1c6a549484084b651418f2
ulzcq <syj9913@ynu.ac.kr>
2022-07-09 00:55:20 +0900

    Feat: Validator 분리 1
    스프링은 검증을 체계적으로 제공하기 위해 Validator 인터페이스를
    제공한다. ItemValidator 클래스를 생성해서 검증 로직을 controller에서 분리하자.
    ItemValidaotr를 스프링 빈으로 주입 받아서 직접 호출한다.
    
    Issues: #8

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -24,6 +24,7 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -154,7 +155,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         //타입 에러가 있으면 그 에러 메시지만 뜨도록
@@ -184,6 +185,24 @@ public class ValidationItemControllerV2 {
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
         }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()){
+            log.info("errors= {}", bindingResult);
+            return "validation/v2/addForm"; //BindingResult는 자동으로 뷰로 넘어가므로 모델로 안넘겨도 된다
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        itemValidator.validate(item, bindingResult); //supports 확인 코드는 생략
 
         //검증에 실패하면 다시 입력 폼으로
         if(bindingResult.hasErrors()){
