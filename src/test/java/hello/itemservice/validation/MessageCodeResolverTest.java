cc9010ec015c70ab7a15027254a5774913b8f1ee
ulzcq <syj9913@ynu.ac.kr>
2022-07-09 00:06:19 +0900

    Feat: 오류 코드와 메시지 처리 3,4
    rejectValue(), reject()는 내부에서 MessageCodesResolver를 사용하여 여러
    오류 코드를 자동으로 생성한다.
    
    타임리프 화면을 렌더링 할 때 th:errors가 실행되고 오류가 있으면 우선
    순위가 가장 높은 오류 메시지 코드를 출력한다.
    
    Issues: #7

---
+++ src/test/java/hello/itemservice/validation/MessageCodeResolverTest.java
@@ -0,0 +1,30 @@
+package hello.itemservice.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MessageCodeResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver(); //에러 코드를 넣으면 메시지 코드 여러개를 생성
+
+    @Test
+    void messageCodesResolverObject(){
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageCodesResolverField(){
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        assertThat(messageCodes).containsExactly(
+                "required.item.itemName",
+                "required.itemName",
+                "required.java.lang.String",
+                "required"
+        );
+    }
+}
